f8c7412152beb8839e84690d290cecaca51dd65a
Doug Chisholm <127129470+DougChisholm@users.noreply.github.com>
2026-04-27 11:54:56 +0100

    Add export_reports.ps1 to run_all.ps1 script

--- run_all.ps1
+++ run_all.ps1
@@ -1,3 +1,4 @@
 powershell -ExecutionPolicy Bypass -File export_schema.ps1
 powershell -ExecutionPolicy Bypass -File export_forms.ps1
+powershell -ExecutionPolicy Bypass -File export_reports.ps1
 powershell -ExecutionPolicy Bypass -File export_vba.ps1
