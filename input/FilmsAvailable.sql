4d922da0217eb2a7279a733ab7feca95f74c6d54
Doug Chisholm <127129470+DougChisholm@users.noreply.github.com>
2026-04-28 10:00:52 +0100

    Delete input directory

--- input/FilmsAvailable.sql
+++
@@ -1,5 +0,0 @@
-﻿SELECT Showing.ShowingID, Film.FilmTitle, Showing.ShowingDate, Showing.ShowingTime
-FROM Film INNER JOIN Showing ON Film.FilmId=Showing.FilmId
-WHERE (((Showing.ShowingDate)>=Date()))
-ORDER BY Film.FilmTitle, Showing.ShowingDate, Showing.ShowingTime;
-
