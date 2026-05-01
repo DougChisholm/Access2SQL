11bbed63479952d922498c6404c21b468d4660d8
Doug Chisholm <127129470+DougChisholm@users.noreply.github.com>
2026-05-01 12:59:58 +0100

    Delete input directory

--- input/FilterResult.sql
+++
@@ -1,4 +0,0 @@
-﻿SELECT StudentResult.ResultId, StudentResult.StudentId, StudentResult.TestId, StudentResult.Mark
-FROM StudentResult
-WHERE (((StudentResult.TestId)=[forms]![ChooseTest]![lstTest]));
-
