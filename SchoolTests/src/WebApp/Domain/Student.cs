65737a0f4a5592b2befdafe3f57809695d96a10f
Doug Chisholm <127129470+DougChisholm@users.noreply.github.com>
2026-05-05 11:46:56 +0100

    Delete SchoolTests directory

--- SchoolTests/src/WebApp/Domain/Student.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApp.Domain;
-
-public class Student
-{
-    public int StudentId { get; set; }
-    public string StudentName { get; set; } = string.Empty;
-    public ICollection<StudentResult> Results { get; set; } = new List<StudentResult>();
-}
--- SchoolTests/src/WebApp/Domain/Test.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApp.Domain;
-
-public class Test
-{
-    public int TestId { get; set; }
-    public string TestDescription { get; set; } = string.Empty;
-    public ICollection<StudentResult> Results { get; set; } = new List<StudentResult>();
-}
