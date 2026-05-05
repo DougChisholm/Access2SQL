48d5fd36615002db83da4ab5f4857b42d12471ce
Doug Chisholm <127129470+DougChisholm@users.noreply.github.com>
2026-05-05 11:47:27 +0100

    Delete input directory

--- input/query_StudentInTest.sql
+++
@@ -1,4 +0,0 @@
-﻿SELECT FilterResult.ResultId, FilterResult.StudentId, Student.StudentName, FilterResult.TestId, FilterResult.Mark
-FROM Student LEFT JOIN FilterResult ON Student.StudentId = FilterResult.StudentId
-ORDER BY Student.StudentName;
-
